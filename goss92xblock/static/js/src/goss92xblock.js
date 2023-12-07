5315e81554f0e21a7ccc9d845c201ac1ac11aa8a
Ilya Goss <goss@corp.ifmo.ru>
2023-12-08 02:37:56 +0300

    Update goss92xblock.js

--- goss92xblock/static/js/src/goss92xblock.js
+++ goss92xblock/static/js/src/goss92xblock.js
@@ -1,17 +1,18 @@
 /* Javascript for goss8XBlock. */
-function Goss92XBlock(runtime, element) {
+function Goss92XBlock(runtime, element, context) {
 
     window.updateScore = function (result) {
         $('.score', element).text(result.score);
         localStorage['score'] = result.score;
         localStorage['score_' + Math.random()] = result.score;
-        localUpdate(result.score); 
+        localUpdate(result.score);
     }
 
     window.handlerUrl = runtime.handlerUrl(element, 'set_score2');
 
-    $('#gosssend').click(buttonClick);
+    init_goss(context); // important!!! 
 
+    $('#gosssend').click(buttonClick);
 
     $(function ($) {
         console.log('*x92***********');
